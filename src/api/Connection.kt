eedb2a27bbaddf35f3b2a0bcb9786a043118488d
cal2195 <calmart101@gmail.com>
2017-11-30 12:55:29 +0000

    Adding client connections!

--- src/api/Connection.kt
+++ src/api/Connection.kt
@@ -1,2 +1,33 @@
 package api
 
+import directoryservice.network.ConnectionAddress
+import directoryservice.network.DirPacket
+import fileserver.packets.NodePacket
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import java.net.Socket
+
+object Connection {
+
+    fun dirRequest(packet: DirPacket, address: ConnectionAddress): DirPacket {
+        val socket = Socket(address.address, address.port)
+        val output = ObjectOutputStream(socket.getOutputStream())
+        println("Sending request to dir server!")
+        output.writeObject(packet)
+        output.flush()
+        println("Awaiting response!")
+        val input = ObjectInputStream(socket.getInputStream())
+        return input.readObject() as DirPacket
+    }
+
+    fun nodeRequest(packet: NodePacket, address: ConnectionAddress): NodePacket {
+        val socket = Socket(address.address, address.port)
+        val output = ObjectOutputStream(socket.getOutputStream())
+        println("Sending request to node server!")
+        output.writeObject(packet)
+        output.flush()
+        println("Awaiting response!")
+        val input = ObjectInputStream(socket.getInputStream())
+        return input.readObject() as NodePacket
+    }
+}
