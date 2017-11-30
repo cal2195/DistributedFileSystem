a585f1bd953efe4054dc9d4a9fde13c0b556c7b1
cal2195 <calmart101@gmail.com>
2017-11-30 12:55:17 +0000

    Adding client bootstrap!

--- src/Bootstrap.kt
+++ src/Bootstrap.kt
@@ -1,3 +1,5 @@
+import client.Client
+import directoryservice.network.ConnectionAddress
 import directoryservice.network.Connections
 import fileserver.Node
 
@@ -10,5 +12,10 @@ fun main(args: Array<String>) {
 
     if (args[0] == "file") {
         val fileServer = Node(args[1], args[2], args[3].toInt())
+    }
+
+    if (args[0] == "fuse") {
+        val client = Client()
+        client.init(args[1], ConnectionAddress(args[2], args[3].toInt()))
     }
 }
